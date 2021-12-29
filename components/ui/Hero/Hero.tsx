204292833d7d180c0e0684c882eb78da08422fdb
wpt2016 <wpt2016@gmail.com>
2021-12-29 22:39:02 +0100

    1st edit

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -11,22 +11,38 @@ interface HeroProps {
 
 const Hero: FC<HeroProps> = ({ headline, description }) => {
   return (
-    <div className="bg-accent-9 border-b border-t border-accent-2">
-      <Container>
-        <div className={s.root}>
-          <h2 className={s.title}>{headline}</h2>
-          <div className={s.description}>
-            <p>{description}</p>
-            <Link href="/">
-              <a className="flex items-center text-accent-0 pt-3 font-bold hover:underline cursor-pointer w-max-content">
-                Read it here
-                <ArrowRight width="20" heigh="20" className="ml-1" />
-              </a>
-            </Link>
-          </div>
-        </div>
-      </Container>
+    <div className="sm:text-base lg:text-left">
+    <div className='relative pl-10 '>
+    <h1 className=" pt-10 text-3xl md:text-4xl tracking-tight font-mediumtext-gray-900 sm:text-5xl md:text-6xl">
+      <span className="block xl:inline "> Welcome to</span> <br/>{' '}
+      <span className=" font-semibold block text-2xl md:text-8xl text-indigo-600 xl:inline">OneColourHoodie</span>
+    </h1>
+    <p className=" mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+      We sell OneColourHoodies
+    </p>
+    <div className=" mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+      <div className="rounded-md shadow">
+        <a
+          href="/search"
+          className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+        >
+          Get started
+        </a>
+      </div>
+
     </div>
+   </div>
+   <div className="lg: lg:inset-y-0 lg:top-0 -mt-80 -z-0">
+              <img
+          className="h-full w-full object-cover sm:bg-cover sm:h-72 md:h-full lg:w-full lg:full"
+          src="./main.jpg"
+          alt=""
+                />
+
+
+    </div>
+    </div>
+
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -40,27 +40,18 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      <Grid variant="filled">
-        {products.slice(0, 3).map((product: any, i: number) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            imgProps={{
-              width: i === 0 ? 1080 : 540,
-              height: i === 0 ? 1080 : 540,
-            }}
-          />
-        ))}
-      </Grid>
+
+      <Hero
+        headline=" "
+        description=""
+      />
+
       <Marquee variant="secondary">
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard key={product.id} product={product} variant="slim" />
         ))}
       </Marquee>
-      <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
-      />
+
       <Grid layout="B" variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard
